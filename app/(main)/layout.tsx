5a6b00a1495e11a4b972993f46e14aab63b2eecc
Noah Love <noah.love@columbia.edu>
2024-04-10 22:26:20 -0400

    Sidebar working for desktop and mobile

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,15 @@
-type Props = {
-  children: React.ReactNode;
-};
+import type { PropsWithChildren } from "react";
 
-const MainLayout = ({ children }: Props) => {
+import { MobileHeader } from "@/components/mobile-header";
+import { Sidebar } from "@/components/sidebar";
+
+const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex" />
       <main className="h-full pt-[50px] lg:pl-[256px] lg:pt-0">
-        <div className="mx-auto h-full max-w-[1056px] pt-6">{children}</div>
+        <div className="bg-red-500 h-full ">{children}</div>
       </main>
     </>
   );
