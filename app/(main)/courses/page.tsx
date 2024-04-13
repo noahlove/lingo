19aff09ddf2b6e38d90371b62f5f1128e8564dd7
Noah Love <noah.love@columbia.edu>
2024-04-12 21:24:25 -0400

    Nairobi spilled water everywhere

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,9 @@
+const CoursesPage = () => {
+  return (
+    <div className="mx-auto h-full max-w-[912px] px-3">
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+    </div>
+  );
+};
+
+export default CoursesPage;
