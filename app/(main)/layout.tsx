6e6bf95434feaa9afa9c86a0d15f9de8c11691cf
Noah Love <noah.love@columbia.edu>
2024-04-12 20:51:28 -0400

    Added database

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -9,7 +9,7 @@ const MainLayout = ({ children }: PropsWithChildren) => {
       <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="h-full pt-[50px] lg:pl-[256px] lg:pt-0">
-        <div className="bg-red-500 h-full ">{children}</div>
+        <div className="max-w-[1056px] mx-auto pt-6 h-full ">{children}</div>
       </main>
     </>
   );
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,21 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
 const LearnPage = () => {
-  return <div>Learn Page</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>My sticky sidebar</StickyWrapper>
+
+      <FeedWrapper>
+        <div className="h-[500px] w-full bg-blue-500" />
+        <div className="h-[500px] w-full bg-blue-500" />
+        <div className="h-[500px] w-full bg-blue-500" />
+        <div className="h-[500px] w-full bg-blue-500" />
+        <div className="h-[500px] w-full bg-blue-500" />
+        <div className="h-[500px] w-full bg-blue-500" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
